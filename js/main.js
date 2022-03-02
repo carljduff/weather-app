699424d0802af28d85e8c8e22176d02d1c80ba5e
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-02 22:38:44 +0000

    update index.html

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 let cards = document.getElementById('card');
 let inputBox = document.getElementById('input');
+let pError = document.getElementById('error');
 let zipEntered = "";
+inputBox.onblur = getZip;
+inputBox.onfocus = init;
 
 function getZip() {
     let zip = inputBox.value; 
     zipEntered = zip;
 }
 
-let getAPI = async function getAPI() {
-    let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipEntered},us&appid=4f0c02c4f644627986534c8f5b3c43d2`;
-    let response = await axios.get(weatherLink);
-    let data = response.data;
-    let condition = data.weather[0].main;
-    let otherInfo = data.weather[0].description;
-    let temperature = data.main.temp;
-    let cityName = data.name;
-    setInnerText('city', cityName);
-    setInnerText('temp', temperature);
-    setInnerText('condition', condition);
-    setInnerText('other', otherInfo);
-    show(cards);
-    console.log(condition)
+let getAPI = async function() {
+    if (isNaN(inputBox.value) === true || inputBox.length < 5) {
+        setInnerText('error', "Please enter a valid Zipcode.");
+    } else {
+        let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipEntered},us&appid=4f0c02c4f644627986534c8f5b3c43d2`;
+        let response = await axios.get(weatherLink);
+        let data = response.data;
+        let condition = data.weather[0].main;
+        let otherInfo = data.weather[0].description;
+        let temperature = data.main.temp;
+        let cityName = data.name;
+        setInnerText('city', cityName);
+        setInnerText('temp', temperature);
+        setInnerText('condition', condition);
+        setInnerText('other', otherInfo);
+        show(cards);
+        hide(pError);
+        console.log(condition)
+        
+
+    }
 }
 
 
@@ -38,18 +48,18 @@ function init() {
     hide(cards);
 }
 
-function validZip() {
-    
-}
+
+
+
 
 
 function weatherDisplay() {
     let button = document.getElementById('button');
     button.addEventListener('click', getAPI);
-    
 }
 
 weatherDisplay();
+
 
 
 
