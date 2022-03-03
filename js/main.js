5cef9e8a2c8158d5b8f15c2ea6342bf9dadec71f
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-03 19:37:41 +0000

    update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ let cards = document.getElementById('card');
 let inputBox = document.getElementById('input');
 let pError = document.getElementById('error');
 let button = document.getElementById('button');
+var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
+    keyboard: false
+  })
 let zipEntered = "";
+//let myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
 inputBox.onblur = getZip;
 inputBox.onfocus = init;
 
@@ -17,17 +21,20 @@ function getZip() {
     zipEntered = zip;
 }
 
-let getAPI = async function() {
+async function getAPI() {
+    try {
     if (isNaN(inputBox.value) === true || inputBox.length < 5) {
         setInnerText('error', "Please enter a valid Zipcode.");
+        show(pError);
     } else {
         let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipEntered},us&appid=4f0c02c4f644627986534c8f5b3c43d2`;
         let response = await axios.get(weatherLink);
         let data = response.data;
         let condition = data.weather[0].main;
         let otherInfo = data.weather[0].icon;
-        let iconLink = "";
-        iconLink.src = `https://openweathermap.org/img/wn/${otherInfo}.png`
+        //let iconLink = `https://openweathermap.org/img/wn/${otherInfo}.png`;
+        let icon = document.getElementById("image");
+        let iconLink = `https://openweathermap.org/img/wn/${otherInfo}.png`;
         let kelvin = data.main.temp;
         let cityName = data.name;
         let celsius = (kelvin - 273.15)
@@ -37,10 +44,17 @@ let getAPI = async function() {
         setInnerText('fahrenheit', Math.round(fahrenheit));
         setInnerText('celsius', Math.round(celsius));
         setInnerText('condition', condition);
-        setInnerText('other', iconLink);
+        //setInnerText('other-body', iconLink);
+        let image = document.getElementById('other-body').appendChild(icon);
+        image.src = iconLink;
         show(cards);
         hide(pError);
+        myModal.hide();
     }
+} catch (e) {
+    myModal.show();
+    console.log(e);
+}
 }
 
 button.onclick = getAPI;
@@ -59,11 +73,7 @@ function init() {
 }
 
 
-/*
-.catch(err => {
-    console.log(err.message);
-})
-*/
+
 
 
 
