22dd5df1125e2f4fde17fcd450caa174ace2b076
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-04 15:05:53 +0000

    update main.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -5,11 +5,12 @@ let storage = document.getElementById('store').innerText;
 let button = document.getElementById('button');
 var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
     keyboard: false
-})
+  })
 let zipEntered = "";
 inputBox.onblur = getZip;
 inputBox.onfocus = init;
 let zips = [];
+
 
 function storeZips() {
     let value = inputBox.value;
@@ -22,74 +23,73 @@ function storeZips() {
         zips.push(value);
     }
 }
+       
+// function pushData() {
+//     let inputText = inputBox.value;
+//     zips.push(inputText);
 
-function getZip() {
-    let zip = inputBox.value; 
-    zipEntered = zip;
-}
+//     let pval = "";
+
+//     for(i = 0; i < zips.length; i++) {
+//         pval = pval + zips[i];
+//     }
+//     document.getElementById('store').innerText = pval;
+   
+// }
 
 function setInnerText(elementID, text) {
     const element = document.getElementById(elementID)
     element.innerText = text;
 }
 
-let store = {
-    condition: [],
-    otherInfo: [],
-    icon: [],
-    iconLink: [],
-    kelvin: [],
-    cityName: []
+function getZip() {
+    let zip = inputBox.value; 
+    zipEntered = zip;
 }
 
 async function getAPI() {
-    let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipEntered},us&appid=4f0c02c4f644627986534c8f5b3c43d2`;
-    let request = await axios.get(weatherLink);
-    let data = request.data;
-    return data;
-}
-
-async function setState() { 
     try {
-        if (isNaN(inputBox.value) === true || inputBox.length < 5) {
-                    setInnerText('error', "Please enter a valid Zipcode.");
-                    show(pError);
+    if (isNaN(inputBox.value) === true || inputBox.length < 5) {
+        setInnerText('error', "Please enter a valid Zipcode.");
+        show(pError);
     } else {
-    data = await getAPI();
-    store.condition = data.weather[0].main;
-    store.otherInfo = data.weather[0].icon;
-    store.kelvin = data.main.temp;
-    store.cityName = data.name;
-    updateState();
+        let weatherLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zipEntered},us&appid=4f0c02c4f644627986534c8f5b3c43d2`;
+        let response = await axios.get(weatherLink);
+        let data = response.data;
+        console.log(data);
+        let condition = data.weather[0].main;
+        let otherInfo = data.weather[0].icon;
+        let icon = document.getElementById("image");
+        let iconLink = `https://openweathermap.org/img/wn/${otherInfo}.png`;
+        let kelvin = data.main.temp;
+        let cityName = data.name;
+        let celsius = (kelvin - 273.15)
+        let fahrenheit = 1.8 * (kelvin - 273) + 32;
+        setInnerText('city', cityName);
+        setInnerText('kelvin', Math.round(kelvin));
+        setInnerText('fahrenheit', Math.round(fahrenheit));
+        setInnerText('celsius', Math.round(celsius));
+        setInnerText('condition', condition);
+        let image = document.getElementById('other-body').appendChild(icon);
+        image.src = iconLink;
+        show(cards);
+        hide(pError);
+        storeZips();
+        myModal.hide();
+        changeBG();
+        
     }
 } catch (e) {
-        myModal.show();
-    }
-}
-
-function updateState() {
-    if (storage == "Please choose a zipcode that has not been chosen") {
-        document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1479&q=80')";
-    } else {
-    let celsius = (data.main.temp - 273.15)
-    let fahrenheit = 1.8 * (data.main.temp - 273) + 32;
-    setInnerText('city', store.cityName);
-    setInnerText('kelvin', Math.round(store.kelvin));
-    setInnerText('fahrenheit', Math.round(fahrenheit));
-    setInnerText('celsius', Math.round(celsius));
-    setInnerText('condition', store.condition);
-    show(cards);
-    hide(pError);
-    storeZips();
-    myModal.hide();
-    let icon = document.getElementById("image");
-    let image = document.getElementById('other-body').appendChild(icon);
-    let iconLink = `https://openweathermap.org/img/wn/${store.otherInfo}.png`;
-    image.src = iconLink;
-    changeBG();
+    myModal.show();
+    console.log(e);
 }
 }
 
+// switch(condition.innerText) {
+//     case 'Clouds':
+//     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1501630834273-4b5604d2ee31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')";
+//     break;
+// }
 function changeBG() {
     if(condition.innerText == 'Clouds') {
         document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1501630834273-4b5604d2ee31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')";
@@ -108,8 +108,7 @@ function changeBG() {
      }
 }
 
-
-button.onclick = setState;
+button.onclick = getAPI;
 
 
 function hide(variable) {
@@ -120,8 +119,20 @@ function show(variable) {
     variable.hidden = false;
 }
 
+
 function init() {
     hide(cards);
-    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1574169208507-84376144848b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1479&q=80')";
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
